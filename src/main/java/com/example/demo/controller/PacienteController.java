e38fa02f9d31b0fb53afaa02c4be82a28ea9043d
Hael1230 <102492080+Hael1230@users.noreply.github.com>
2023-04-17 10:59:06 -0500

    Add files via upload
    Eliminar Paciente

---
+++ src/main/java/com/example/demo/controller/PacienteController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entitis.Paciente;
+import com.example.demo.repository.PacienteRepsitory;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PacienteController {
+
+    @Autowired
+	PacienteRepsitory pacienteRepsitory;
+
+    @GetMapping
+	public List<Paciente> getPacienteAll() {
+
+		return pacienteRepsitory.findAll();
+	}
+
+    @DeleteMapping("/{id}")
+	public Paciente deletePacientesbyId(@PathVariable Integer id) {
+		
+		Optional<Paciente> paciente = pacienteRepsitory.findById(id);
+		
+		if (paciente.isPresent()) {
+			
+			final Paciente pacienteReturn = paciente.get();
+			
+			pacienteRepsitory.deleteById(id);
+			
+			return pacienteReturn;
+		}
+		
+		return null;
+
+	}
+    
+}
